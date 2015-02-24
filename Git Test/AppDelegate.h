b0b0a3ac9bbb18325d5bd6ec71bac34e6e0044da
Laura Gonzalez <lauragonzalez@pc-Laura.andalucia.org>
2015-02-24 09:21:56 +0100

    Initial Commit

---
+++ Git Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git Test
+//
+//  Created by Laura Gonzalez on 24/2/15.
+//  Copyright (c) 2015 TURASA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git Test
+//
+//  Created by Laura Gonzalez on 24/2/15.
+//  Copyright (c) 2015 TURASA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
