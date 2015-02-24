c0bfeb17785f169f141addaa38ce8fea402ba011
Marlamiir <laura@andalucia.org>
2015-02-24 10:23:18 +0100

    added a test class

---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Laura Gonzalez on 24/2/15.
+//  Copyright (c) 2015 TURASA. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
